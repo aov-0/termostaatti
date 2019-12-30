c2bbb48148cd48e75de4267be8ad50aee02ac93a
aov-0 <akustma2@hotmail.com>
2019-12-30 13:53:42 +0200

    aloitus

---
+++ termostaatti.ino
@@ -0,0 +1,13 @@
+//Määrittää pinneille kuvaavat nimet, esim. ANTURI lämpöanturille
+#define ANTURI A0
+#define POTENTIO A1
+#define RELE 11
+#define LEDPUNAINEN 12
+#define LEDVIHREA 13
+
+
+void setup(){
+}
+
+void loop() {
+ }

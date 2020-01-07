a4c5ce9ee08f38b8eb37249f6a74f283dee9664d
Aku Vilkki <aku.vilkki@ictpro.labra>
2020-01-07 13:30:51 +0200

    lisätty toimintoja

--- termostaatti.ino
+++ termostaatti.ino
@@ -1,13 +1,23 @@
 //Määrittää pinneille kuvaavat nimet, esim. ANTURI lämpöanturille
 #define ANTURI A0
 #define POTENTIO A1
-#define RELE 11
+#define FAN 11
 #define LEDPUNAINEN 12
 #define LEDVIHREA 13
 
+int LUKEMA;
+int ASETUS;
 
 void setup(){
+  pinMode(LEDPUNAINEN, OUTPUT);
+  pinMode(LEDVIHREA, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  LUKEMA = analogRead(ANTURI);
+  Serial.println(LUKEMA);
+  ASETUS = analogRead(POTENTIO);
+  Serial.println(ASETUS);
+  delay(500);
  }

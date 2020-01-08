7123900364e9200df47f0dcd6fcf71d47518d104
Aku Vilkki <aku.vilkki@ictpro.labra>
2020-01-08 10:33:37 +0200

    lisätty lämpötilan mittaus

--- termostaatti.ino
+++ termostaatti.ino
@@ -2,22 +2,31 @@
 #define ANTURI A0
 #define POTENTIO A1
 #define FAN 11
-#define LEDPUNAINEN 12
-#define LEDVIHREA 13
+#define LEDPUN 12
+#define LEDVIH 13
 
 int LUKEMA;
 int ASETUS;
 
 void setup(){
-  pinMode(LEDPUNAINEN, OUTPUT);
-  pinMode(LEDVIHREA, OUTPUT);
+  pinMode(LEDPUN, OUTPUT);
+  pinMode(LEDVIH, OUTPUT);
+  pinMode(FAN, OUTPUT);
+  analogWrite(FAN, 0);
   Serial.begin(9600);
 }
 
 void loop() {
   LUKEMA = analogRead(ANTURI);
-  Serial.println(LUKEMA);
+  float ASTE = (-9.00/70.00)*LUKEMA+91;
+  Serial.print(ASTE);
+  Serial.println(" astetta");
   ASETUS = analogRead(POTENTIO);
   Serial.println(ASETUS);
   delay(500);
+  if(ASTE > 23){
+    analogWrite(FAN, 255);
+  }else if(ASTE < 23){
+    analogWrite(FAN, 0);
+  }
  }

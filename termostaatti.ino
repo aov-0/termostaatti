67254c7066015b0678a6100cef578d1d1dbd1106
Aku Vilkki <aku.vilkki@ictpro.labra>
2020-01-08 14:43:55 +0200

    PWM ohjausta tuulettimelle

--- termostaatti.ino
+++ termostaatti.ino
@@ -5,8 +5,6 @@
 #define LEDPUN 12
 #define LEDVIH 13
 
-//esimerkki muokkaus
-
 //Selvyyden vuoksi ledin sytyttämiselle/sammuttamiselle omat nimitykset
 #define LEDON HIGH
 #define LEDOFF LOW
@@ -14,6 +12,9 @@
 int LUKEMA;
 int ASETUS;
 int PWM;
+
+//Halutut toiminnot löytyy ohjelmasta mutta vaatii säätämistä
+//
 
 void setup(){
   pinMode(LEDPUN, OUTPUT);
@@ -30,19 +31,26 @@ void loop() {
   Serial.print(ASTE);
   Serial.println(" astetta");
   ASETUS = analogRead(POTENTIO);
-  ASETUS = map(ASETUS, 0, 1023, 16, 30);
+  ASETUS = map(ASETUS, 0, 1023, 160, 300);    //160 & 300 potentiometrin halutut ääriasennot (alunperin minimi 16 astetta, max 30 astetta), lisättiin nollat perään jotta saatiin "askellusta" lisää.
   Serial.print("Raja-arvo: ");
   Serial.print(ASETUS);
   Serial.println(" astetta");
   delay(1000);
   digitalWrite(LEDVIH, LEDON);
-  //PWM = map(FAN, 60, 255, 1, 7);
+  ASTE*=10;
+  PWM = map(ASTE - ASETUS, 10, 70, 60, 255);
+  Serial.println(PWM);
+  if(PWM>0){
+    analogWrite(FAN, PWM);
+  }else{
+    analogWrite(FAN, 0);
+  }
   if(ASTE > ASETUS){
     digitalWrite(LEDVIH, LEDOFF);
     digitalWrite(LEDPUN, LEDON);
-    analogWrite(FAN, 180);
-  }else if(ASTE < ASETUS + 2){
+    //analogWrite(FAN, PWM);
+  }else if(ASTE < ASETUS - 3){
     digitalWrite(LEDPUN, LEDOFF);
-    analogWrite(FAN, 0);
+    //analogWrite(FAN, 0);
   }
  }
